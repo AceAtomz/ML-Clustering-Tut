35a5b90f68f201ab2d2a8683e58ea5623a52f4da
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-05-18 16:40:07 +0200

    Create #Quesion 1.py

---
+++ #Quesion 1.py
@@ -0,0 +1,10 @@
+#Quesion 1
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+import math
+
+
+print("skelly")
