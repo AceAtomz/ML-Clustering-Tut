e63104c2d7637f737fe48959cc4a5489695a9513
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-05-19 17:23:59 +0200

    update obj

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ np.random.seed(4109) #new seed for new initialization of centres
 
 kM = kMeans(clusters, 2)
 for i in range(5):
-    cluster1, cluster2 = kM.getNewClusters() #, cluster3, cluster4
+    cluster1, cluster2 = kM.getNewClusters()
     centres = kM.getNewCentre()
     obj = kM.getObjectiveFunction()
 
@@ -24,23 +24,12 @@ x11 = cluster1[:, 0]
 x12 = cluster1[:, 1]
 x21 = cluster2[:, 0]
 x22 = cluster2[:, 1]
-"""
-x31 = cluster3[:, 0]
-x32 = cluster3[:, 1]
-x41 = cluster4[:, 0]
-x42 = cluster4[:, 1]
-"""
+
 fig, ax1 = plt.subplots(figsize=(7,5))
 ax1.scatter(x11, x12,  color = 'blue')
 ax1.scatter(centres[0][0], centres[0][1], color='blue', marker="s", s=150)
 ax1.scatter(x21, x22,  color = 'red') #red
 ax1.scatter(centres[1][0], centres[1][1], color='red', marker="s", s=150)
-"""
-ax1.scatter(x31, x32,  color = 'hotpink') #hotpink
-ax1.scatter(centres[2][0], centres[2][1], color='hotpink', marker="s", s=150)
-ax1.scatter(x41, x42,  color = 'lime') #lime
-ax1.scatter(centres[3][0], centres[3][1], color='lime', marker="s", s=150)
-"""
 ax1.set_title('Fifth Iteration with k=2 Clusters  (OBJ = ' + str(obj) + ')')
 ax1.set_xlabel("x1", fontsize=15)
 ax1.set_ylabel("x2", fontsize=15)
