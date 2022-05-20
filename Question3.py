9cf700c95bbae4cd155fd582d8ff0bdd90528db2
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-05-20 02:59:33 +0200

    huh

--- Question3.py
+++ Question3.py
@@ -7,23 +7,27 @@ originalImg = plt.imread("peppers1.jpg")
 img = np.reshape(originalImg, (originalImg.shape[0]*originalImg.shape[1], originalImg.shape[2]))
 
 np.random.seed(4109) #new seed for new initialization of centres
-k=2
+k=4
 kM = kMeans(img, k)
 obj=0.0
 
 for i in range(10):
-    cluster1, cluster2 = kM.getNewClusters()
+    cluster1, cluster2, cluster3, cluster4 = kM.getNewClusters()
     centres = kM.getNewCentre()
+    """
     newobj = kM.getObjectiveFunction()
     if(newobj==obj):
         print(i)
         break
     else:
         obj = newobj
+    """
 
 cluster1 = np.array(cluster1, dtype=np.int16)
 cluster2 = np.array(cluster2, dtype=np.int16)
-print(centres)
+cluster3 = np.array(cluster3, dtype=np.int16)
+cluster4 = np.array(cluster4, dtype=np.int16)
+
 for i in range(img.shape[0]):
     for j in range(cluster1.shape[0]):
         if(img[i][0]==cluster1[j][0] and img[i][1]==cluster1[j][1] and img[i][2]==cluster1[j][2]):
@@ -32,6 +36,14 @@ for i in range(img.shape[0]):
     for j in range(cluster2.shape[0]):
         if(img[i][0]==cluster2[j][0] and img[i][1]==cluster2[j][1] and img[i][2]==cluster2[j][2]):
             img[i] = centres[1]
+            break
+    for j in range(cluster3.shape[0]):
+        if(img[i][0]==cluster3[j][0] and img[i][1]==cluster3[j][1] and img[i][2]==cluster3[j][2]):
+            img[i] = centres[2]
+            break
+    for j in range(cluster4.shape[0]):
+        if(img[i][0]==cluster4[j][0] and img[i][1]==cluster4[j][1] and img[i][2]==cluster4[j][2]):
+            img[i] = centres[3]
             break
         
 img = np.reshape(img, (originalImg.shape[0], originalImg.shape[1], originalImg.shape[2]))
--- kMeans.py
+++ kMeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.cluster import cluster_optics_dbscan
 
 class kMeans():
     def __init__(self, clusters, k):
@@ -24,39 +25,57 @@ class kMeans():
         self.getDistances()
 
         p1 = np.amin(self.distances, axis=1)
-        self.pos = np.empty((self.clusters.shape[0]), np.int8)
+        self.pos = np.empty((self.clusters.shape[0]), np.int16)
         temp0 = np.empty((0, 3))
         temp1 = np.empty((0, 3))
+        temp2 = np.empty((0, 3))
+        temp3 = np.empty((0, 3))
         
         for i in range(self.clusters.shape[0]):
             for j in range(self.k):
                 if(p1[i]==self.distances[i][j]):
                     self.pos[i] = j
-
+       
         for i in range(self.clusters.shape[0]):
             if(self.pos[i]==0):
                 temp0 = np.append(temp0, np.array([self.clusters[i]]), axis=0)
             elif(self.pos[i]==1):
                 temp1 = np.append(temp1, np.array([self.clusters[i]]), axis=0)
+            elif(self.pos[i]==2):
+                temp2 = np.append(temp2, np.array([self.clusters[i]]), axis=0)
+            elif(self.pos[i]==3):
+                temp3 = np.append(temp3, np.array([self.clusters[i]]), axis=0)
+
         self.cluster0 = temp0
         self.cluster1 = temp1
-        self.clusters = np.concatenate((self.cluster0,self.cluster1), axis=0)
+        self.cluster2 = temp2
+        self.cluster3 = temp3
+        self.clusters = np.concatenate((self.cluster0,self.cluster1,self.cluster2,self.cluster3), axis=0)
 
-        return self.cluster0, self.cluster1
+        return self.cluster0, self.cluster1,self.cluster2, self.cluster3
 
     def getNewCentre(self):
         self.centres[0] = np.average(self.cluster0, axis=0)
         self.centres[1] = np.average(self.cluster1, axis=0)
+        self.centres[2] = np.average(self.cluster2, axis=0)
+        self.centres[3] = np.average(self.cluster3, axis=0)
         return self.centres
 
     def getObjectiveFunction(self):
         self.obj = 0.0
-
+        
         for i in range(self.cluster0.shape[1]):
             self.obj += np.sum(np.square(self.cluster0[i]-self.centres[0]))
         
         for i in range(self.cluster1.shape[1]):
             self.obj += np.sum(np.square(self.cluster1[i]-self.centres[1]))
+
+        for i in range(self.cluster2.shape[1]):
+            self.obj += np.sum(np.square(self.cluster2[i]-self.centres[2]))
+
+        for i in range(self.cluster3.shape[1]):
+            self.obj += np.sum(np.square(self.cluster3[i]-self.centres[3]))
+
         self.obj = self.obj/self.clusters.shape[0]
 
         return self.obj
